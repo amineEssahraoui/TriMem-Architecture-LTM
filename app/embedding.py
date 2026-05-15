aaf66ca2a14aca90b345c158633aee005149747b
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-05-15 18:44:18 +0100

    File to Transforms text into a numerical vector

---
+++ app/embedding.py
@@ -0,0 +1,16 @@
+import ollama
+from app.config import settings
+
+def get_embedding(text : str) -> list[float]: # Take a text and return emmbedding vector
+    """
+    Transforms text into a numerical vector (embedding) using nomic-embed-text.
+    """
+    try:
+        response = ollama.embeddings(
+            model=settings.EMBEDDING_MODEL,
+            prompt=text
+        )
+        return response['embedding']
+    except Exception as e:
+        print(f"Error generating embedding: {e}")
+        return []
