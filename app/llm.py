67276511d36ef7b47bc90530bb0e9b0f6466c4d2
Amine Essahraoui <amine.essahraoui04@gmail.com>
2026-05-15 18:34:48 +0100

    Implement generate_response function to take prompt and system_prompt if available and return llm response

---
+++ app/llm.py
@@ -0,0 +1,25 @@
+import ollama
+from app.config import settings
+
+def generate_response(prompt : str, system_prompt: str = "") -> str: # Empty System_prompt by default !
+    """
+    Sends a prompt to the LLM and returns its generated response.
+    """
+    messages = []
+    if system_prompt: 
+        messages.append({"role": "system", "content" : system_prompt}) # Add system prompte if available
+
+    messages.append({"role": "user", "content": prompt}) # Add user prompte
+
+    try: 
+        response = ollama.chat(
+            model = settings.LLM_MODEL,
+            messages = messages
+        )
+        return response['embedding']
+    except Exception as e: 
+        print(f"Error generating embedding: {e}")
+        return []
+
+    
+
